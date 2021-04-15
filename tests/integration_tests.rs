ec83d1a45818ce1e52ac83a52b3e7037c542a080
Christian <christianw@scribd.com>
2021-04-15 19:27:49 -0400

    Add dummy AWS credentials for localstack tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -116,6 +116,8 @@ fn setup() {
     INIT.call_once(|| {
         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("error")).init();
         env::set_var("AWS_ENDPOINT_URL", LOCALSTACK_ENDPOINT);
+        env::set_var("AWS_ACCESS_KEY_ID", "test");
+        env::set_var("AWS_SECRET_ACCESS_KEY", "test");
     });
 }
 
