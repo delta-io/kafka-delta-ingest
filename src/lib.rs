31448c27e9437f1c2e9f600b2282e4c5a0a313b0
xianwill <christianwilliams79@gmail.com>
2021-11-12 09:49:17 -0500

    Remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -572,13 +572,7 @@ impl IngestProcessor {
         let dlq = dead_letter_queue_from_options(&opts).await?;
         let transformer = Transformer::from_transforms(&opts.transforms)?;
         let table = delta_helpers::load_table(table_uri, HashMap::new()).await?;
-        // TODO: The coercion_tree needs to be replaced when a schema update is received.
-        // TODO: If the delta schema is None, return an `IngestError` instead of using expect.
         let coercion_tree = coercions::create_coercion_tree(&table.get_metadata()?.schema);
-        // .schema()
-        // .expect("Delta table must have a schema")
-        // .clone(),
-        // );
         let delta_writer = DataWriter::for_table(&table, HashMap::new())?;
 
         Ok(IngestProcessor {
