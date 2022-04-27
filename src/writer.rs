e7884321daa6aa20468fd385481c7b74df09d35b
xianwill <christianwilliams79@gmail.com>
2022-04-27 13:19:23 -0400

    Refactor writer::apply_null_counts to reduce nesting

--- src/writer.rs
+++ src/writer.rs
@@ -676,42 +676,54 @@ fn apply_null_counts(
                 return;
             }
 
-            match column.data_type() {
-                // Recursive case
-                DataType::Struct(_) => {
-                    let col_struct = null_counts
-                        .entry(key)
-                        .or_insert_with(|| ColumnCountStat::Column(HashMap::new()));
-
-                    match col_struct {
-                        ColumnCountStat::Column(map) => {
-                            apply_null_counts(
-                                partition_columns,
-                                as_struct_array(column),
-                                map,
-                                nest_level + 1,
-                            );
-                        }
-                        _ => unreachable!(),
-                    }
-                }
-                // Base case
-                _ => {
-                    let col_struct = null_counts
-                        .entry(key.clone())
-                        .or_insert_with(|| ColumnCountStat::Value(0));
-
-                    match col_struct {
-                        ColumnCountStat::Value(n) => {
-                            let null_count = column.null_count() as DeltaDataTypeLong;
-                            let n = null_count + *n;
-                            null_counts.insert(key, ColumnCountStat::Value(n));
-                        }
-                        _ => unreachable!(),
-                    }
-                }
-            }
+            apply_null_counts_for_column(partition_columns, null_counts, nest_level, column, field);
         });
+}
+
+fn apply_null_counts_for_column(
+    partition_columns: &[String],
+    null_counts: &mut HashMap<String, ColumnCountStat>,
+    nest_level: i32,
+    column: &&Arc<dyn Array>,
+    field: &Field,
+) {
+    let key = field.name().to_owned();
+
+    match column.data_type() {
+        // Recursive case
+        DataType::Struct(_) => {
+            let col_struct = null_counts
+                .entry(key)
+                .or_insert_with(|| ColumnCountStat::Column(HashMap::new()));
+
+            match col_struct {
+                ColumnCountStat::Column(map) => {
+                    apply_null_counts(
+                        partition_columns,
+                        as_struct_array(column),
+                        map,
+                        nest_level + 1,
+                    );
+                }
+                _ => unreachable!(),
+            }
+        }
+        // Base case
+        _ => {
+            let col_struct = null_counts
+                .entry(key.clone())
+                .or_insert_with(|| ColumnCountStat::Value(0));
+
+            match col_struct {
+                ColumnCountStat::Value(n) => {
+                    let null_count = column.null_count() as DeltaDataTypeLong;
+                    let n = null_count + *n;
+                    null_counts.insert(key, ColumnCountStat::Value(n));
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
 
 fn apply_min_max_for_column(
