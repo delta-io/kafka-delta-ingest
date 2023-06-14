67269cc0e4e2afc9a4a7fc7d67f529a5dc4f8714
R. Tyler Croy <rtyler@brokenco.de>
2023-06-14 10:55:35 -0700

    Exclude DynamoDB locking mechanisms for `azure` builds

--- src/dead_letters.rs
+++ src/dead_letters.rs
@@ -4,8 +4,10 @@ use chrono::prelude::*;
 use core::fmt::Debug;
 use deltalake::parquet::errors::ParquetError;
 use deltalake::{DeltaTable, DeltaTableError};
+#[cfg(feature = "s3")]
 use dynamodb_lock::dynamo_lock_options;
 use log::{error, info, warn};
+#[cfg(feature = "s3")]
 use maplit::hashmap;
 use rdkafka::message::BorrowedMessage;
 use serde::{Deserialize, Serialize};
@@ -15,6 +17,7 @@ use std::collections::HashMap;
 use crate::{transforms::TransformError, writer::*};
 use deltalake::checkpoints::CheckpointError;
 
+#[cfg(feature = "s3")]
 mod env_vars {
     pub(crate) const DEAD_LETTER_DYNAMO_LOCK_PARTITION_KEY_VALUE: &str =
         "DEAD_LETTER_DYNAMO_LOCK_PARTITION_KEY_VALUE";
@@ -246,10 +249,14 @@ impl DeltaSinkDeadLetterQueue {
     ) -> Result<Self, DeadLetterQueueError> {
         match &options.delta_table_uri {
             Some(table_uri) => {
+                #[cfg(feature = "s3")]
                 let opts = hashmap! {
                     dynamo_lock_options::DYNAMO_LOCK_PARTITION_KEY_VALUE.to_string() => std::env::var(env_vars::DEAD_LETTER_DYNAMO_LOCK_PARTITION_KEY_VALUE)
                     .unwrap_or_else(|_| "kafka_delta_ingest-dead_letters".to_string()),
                 };
+                #[cfg(feature = "azure")]
+                let opts = HashMap::default();
+
                 let table = crate::delta_helpers::load_table(table_uri, opts.clone()).await?;
                 let delta_writer = DataWriter::for_table(&table, opts)?;
 
