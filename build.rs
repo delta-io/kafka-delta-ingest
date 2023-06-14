216f763191107b05c7970f763db8a4ff7384723b
R. Tyler Croy <rtyler@brokenco.de>
2023-06-14 07:44:56 -0700

    Copy test fixtures in Rust rather than relying on shell scripts

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 #[cfg(not(any(feature = "s3", feature = "azure")))]
 compile_error!(
-    "Either the \"s3\" or the \"azure\" feature must be built to compile kafka-delta-ingest"
+    "Either the \"s3\" or the \"azure\" feature must be enabled to compile kafka-delta-ingest"
 );
 fn main() {}
